c2d508e20348fbd7e1ae65d7b4eae320c667f0c9
Mehmet <mhmtsytrk@gmail.com>
2020-10-28 20:50:02 +0300

    Last Version of master

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,25 +1,19 @@
 package com.vytrack.runners;
 
 
-import io.cucumber.java.StepDefinitionAnnotations;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                "html:target/default-html-reports",
-                "rerun:target/rerun.txt"
-        },
-
+                  "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,  // it control only steps are here  or not
+        dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
-
-
 }
